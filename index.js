bdd90f95829c28c8a5ab830d831d3bb6a8255a84
John Smith <unknown.prop@gmail.com>
2018-06-23 11:36:18 +0900

    Benchmark Refactor

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 
 if (cluster.isMaster) {
   cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
 } else {
   const express = require("express");
+  const crypto = require("crypto");
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there");
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
   });
 
   app.get("/fast", (req, res) => {
